6dbb61f5ad503b931fefc0ee677e5c45cd8cbd06
Mallory A. Ballinger <ballingermallory@gmail.com>
2020-12-19 12:49:12 -0700

    Added some code

---
+++ code/weekly_phenotypes.R
@@ -0,0 +1,12 @@
+#!/usr/bin/env Rscript --vanilla
+
+# name: weekly_phenotypes_graphed.R
+#
+# Plot evolved and plastic differences in weekly body weights and tail lengths
+#
+# input:
+#       - data/XXXX
+# output:
+#       - data/XXXX
+
+library(tidyverse)

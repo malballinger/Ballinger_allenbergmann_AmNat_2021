1a4a33d2fe65507d9d7da260579e7e903ff9c738
Mallory A. Ballinger <ballingermallory@gmail.com>
2021-04-05 18:27:24 -0600

    Updated data READMEs

---
+++ code/clean_WeeklyPhenotypes.R
@@ -0,0 +1,45 @@
+#!/usr/bin/env Rscript --vanilla
+
+##############################################################
+# Author: Mallory A. Ballinger
+# Script first created: 12-Feb-2021
+# Script last updated:  05-Apr-2021
+
+
+# This script cleans the metadata file: data/raw/weekly_metadata_RAW_2021-02-11.csv.
+# The generated, cleaned dataset is used for downstream analyses.
+
+
+##############################################################
+# Required packages
+##############################################################
+
+rm(list = ls()) # clear R's environment
+library(tidyverse)
+library(here)
+
+##############################################################
+# Import & clean data
+##############################################################
+
+WeeklyMetaData <- read_csv(here("data/raw/weekly_metadata_RAW_2021-02-11.csv")) %>%
+  select(-DOB, -DateMeasured) %>%
+  rename("Tail_Length_mm" = "TailLength_mm",
+         "Body_Weight_g" = "BodyWeight_g") %>%
+  filter(Population == "BRAZIL" | Population == "NEW_YORK") %>% # only keep parental populations (remove F1 hybrids)
+  filter(Generation == "N11" | Generation == "N12") %>%
+  mutate(Sex = fct_recode(Sex, "Female" = "F", "Male" = "M")) %>% # spells out males and females
+  mutate(Environment = fct_recode(Environment, "Cold" = "COLD", "Warm" = "RT")) %>%
+  mutate(Line = fct_recode(Line, "MANA" = "193x255", "SARA" = "19x13",
+                           "MANB" = "222x254", "SARB" = "82x81")) %>% # gives each line the "published"/JAX name
+  mutate(Population = fct_recode(Population, "Brazil" = "BRAZIL", "NewYork" = "NEW_YORK")) %>% # modifies names of populations
+  mutate(PopEnv = paste(Population, Environment, sep = "_")) %>%
+  mutate(PopEnv = fct_recode(PopEnv, "Brazil - Warm" = "Brazil_Warm", "Brazil - Cold" = "Brazil_Cold",
+                             "New York - Warm" = "NewYork_Warm", "New York - Cold" = "NewYork_Cold"))
+
+# Sample size of dataset
+FullSampleSize <- WeeklyMetaData %>% summarise(N=n_distinct(Mouse_ID)) %>% pull(N)
+# n = 84
+
+write.csv(WeeklyMetaData, file = "results/tables/WeeklyPhenotypeData.csv", row.names = TRUE)
+write.csv(WeeklyMetaData, file = "data/processed/WeeklyPhenotypeData.csv", row.names = TRUE)
--- data/raw/README.md
+++ data/raw/README.md
@@ -1,13 +1,14 @@
-Raw data files stored here are not altered. Files that are modified are saved here: *../processed/*.
+###Raw data files stored here are not altered. Files that are modified are saved here: *../processed/*.###
 
 
-<u>Raw data files used for Figure 1 and Figure S1 include:</u>
+#####Raw data files used for Figure 1 and Figure S1 include:#####
 * <strong>VertNet_Mus_specimen_20201013.tsv</strong>
 * <strong>EnvAdapProj_Nachman_Arctos_transects_2021_03_15.csv</strong>
 
 
-Raw data files used for Figure 2 and Fig Sxx include:
+#####Raw data files used for Figure 2 and Fig Sxx include:#####
+* <strong>colony_metadata_RAW.xlsx</strong>
 
 
-
-Raw data files used for Figure 3 and Fig Sxx include:
+#####Raw data files used for Figure 3 and Fig Sxx include:#####
+* <strong>weekly_metadata_RAW_2021-02-11.csv</strong>
